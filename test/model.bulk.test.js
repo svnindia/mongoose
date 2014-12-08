eba1ec39bc6f56b0b9950560f42abaddb97b61a1
Valeri Karpov <valkar207@gmail.com>
2014-12-08 16:24:34 -0500

    Basic setup for bulk API #2366

---
+++ test/model.bulk.test.js
@@ -0,0 +1,42 @@
+/**
+ * Test dependencies.
+ */
+
+var start = require('./common');
+var mongoose = start.mongoose;
+var assert = require('assert');
+
+describe('Mongoose Bulk API wrapper', function() {
+  var db;
+
+  beforeEach(function() {
+    db = start();
+  });
+
+  afterEach(function(done) {
+    db.close(done);
+  });
+
+  it('unordered inserts work', function(done) {
+    var omeletteSchema = mongoose.Schema({ topping: String });
+
+    var Omelette = db.model('gh-2399-1', omeletteSchema);
+
+    db.on('connected', function() {
+    Omelette.
+      initializeUnorderedBulkOp().
+      insert({ topping: 'bacon' }).
+      insert({ topping: 'sausage' }).
+      execute(function(error) {
+        assert.ifError(error);
+        Omelette.find({}).sort({ topping: 1 }).exec(function(error, toppings) {
+          assert.ifError(error);
+          assert.equal(2, toppings.length);
+          assert.equal('bacon', toppings[0].topping);
+          assert.equal('sausage', toppings[1].topping);
+          done();
+        });
+      });
+    });
+  });
+});
